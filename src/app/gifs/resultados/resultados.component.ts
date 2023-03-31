f2b2f0fe698277a37c58850c23a3cdccbdea18a2
izrael90 <izraeldeveloper@gmail.com>
2023-03-31 16:44:00 -0600

    Fin aplicacion

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles: [
+  ]
+})
+export class ResultadosComponent {
+
+  get resultados(){
+    return this.gifsSerfice.resultados;
+  }
+  
+  constructor(private gifsSerfice: GifsService) { }
+
+
+
+}
